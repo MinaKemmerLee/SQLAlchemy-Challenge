2774db40fe09747f6f676b84f80277a245db3de3
MinaKemmerLee <minakl.official@gmail.com>
2023-05-25 23:37:33 -0700

    additional update on files

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -34,6 +34,7 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def home():
+    # List all routes 
     return(
         f"Welcome to the Hawaii Climate API Homepage!<br/>"
         f"The Available Routes are listed below: <br/>"
@@ -46,6 +47,7 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def prcp():
+    # Create our session (link) from Python to the DB
     session= Session(engine)
 
     the_most_recent_date= dt.date(2017,8,23)
@@ -55,33 +57,45 @@ def prcp():
     data= [measurement.date, measurement.prcp]
     year1results = session.query(*data).filter(measurement.date >= one_year_prior).all()
 
+    # Close session
     session.close()
 
+    # Create a list with the Prcp Data
     prcp_data= []
     for date, rain in year1results:
         prcp_dict= {}
         prcp_dict["Date"]= date
         prcp_dict["Prcp"]= rain
         prcp_data.append(prcp_dict)
+    
+    # jsonify response
     return (jsonify(prcp_data))
 
 
 @app.route("/api/v1.0/stations")
 def stations():
+    # Create our session (link) from Python to the DB
     session= Session(engine)
 
+    # Query all needed info
     data= [station.station]
     allstations = session.query(*data).all()
 
+    # Close session
     session.close()
 
+    # Create a Station list
     station_list = [result[0] for result in allstations]
+
+    # jsonify response
     return (jsonify(station_list))
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # Query all needed info
     most_active_station = session.query(measurement.station).\
         group_by(measurement.station).\
         order_by(func.count(measurement.station).desc()).\
@@ -95,24 +109,29 @@ def tobs():
         filter(measurement.station == most_active_station[0]).\
         filter(measurement.date >= year_prior_to_last_date).all()
 
+    # Close session
     session.close()
 
+    # Create a list with the TOBS
     tobs_list = []
     for date, tobs in tobs_data:
         tobs_dict = {"Date": date, "TOBS": tobs}
         tobs_list.append(tobs_dict)
 
+    # jsonify response
     return jsonify(tobs_list)
 
 
 @app.route("/api/v1.0/<start>")
 def temperature_summary_start(start):
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Query the minimum, average, and maximum temperatures for dates greater than or equal to the start date
+    # Query all needed info
     results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
         filter(measurement.date >= start).all()
 
+    # Close session
     session.close()
 
     # Create a dictionary with the temperature summary
@@ -122,17 +141,19 @@ def temperature_summary_start(start):
         "TAVG": results[0][1],
         "TMAX": results[0][2]
     }
-
+    # jsonify response
     return (jsonify(temperature_summary_1))
 
 @app.route("/api/v1.0/<start>/<end>")
 def temperature_summary_range(start, end):
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Query the minimum, average, and maximum temperatures for dates within the start-end range
+    # Query all needed info
     results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
         filter(measurement.date >= start).filter(measurement.date <= end).all()
 
+    # Close session
     session.close()
 
     # Create a dictionary with the temperature summary
@@ -143,8 +164,9 @@ def temperature_summary_range(start, end):
         "TAVG": results[0][1],
         "TMAX": results[0][2]
     }
-
+    # jsonify response
     return (jsonify(temperature_summary_2))
 
+# Run app
 if __name__ == "__main__":
     app.run(debug=True)
